56fff5dd9fcf2c6ae92dfc862f3b12e28c3253d3
Titus Baxter Bokosi <titusbokosi@gmail.com>
2026-03-27 18:48:56 +0200

    feat: implimented house listing routes #6

--- src/routes/houseListingRoutes.mjs
+++ src/routes/houseListingRoutes.mjs
@@ -5,5 +5,8 @@ const houseListingRoutes = express.Router();
 
 houseListingRoutes.get('/', houseController.getHouses);
 houseListingRoutes.post('/', houseController.createHouse);
+houseListingRoutes.get('/:id', houseController.getHouseById);
+houseListingRoutes.patch('/:id', houseController.updateHouse);
+houseListingRoutes.delete('/:id', houseController.deleteHouse);
 
 export default houseListingRoutes;
