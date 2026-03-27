6d3b35137f0899e4bc2e3e6c511a59c2df1e72b1
Titus Baxter Bokosi <titusbokosi@gmail.com>
2026-03-27 10:51:27 +0200

    feat: implimented house booking #7

---
+++ src/models/HouseBooking.mjs
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const houseBooking = new mongoose.Schema({
+  house: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'House',
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'cancelled'],
+    default: 'pending',
+  },
+  payment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Payment',
+    default: null,
+  },
+});
+
+const Booking = mongoose.model('Booking', houseBooking);
+
+export default Booking;
