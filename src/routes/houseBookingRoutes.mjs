081519a2ccfb72094c32d8f25e27898eda0ea3b9
Titus Baxter Bokosi <titusbokosi@gmail.com>
2026-04-03 10:50:13 +0200

    fixed : house listing controlers, routes, added houseupdate validator #6

--- src/routes/houseBookingRoutes.mjs
+++ src/routes/houseBookingRoutes.mjs
@@ -1,32 +1,12 @@
-import express from 'express'
-import * as houseBookingController from '../controllers/houseBookingController.mjs'
+import express from 'express';
+import * as houseBookingController from '../controllers/houseBookingController.mjs';
 
 const houseBookingRoutes = express.Router();
 
-houseBookingRoutes.use('/', houseBookingController.getBookings)
+houseBookingRoutes.use('/', houseBookingController.getBookings);
+houseBookingRoutes.post('/', houseBookingController.createBook);
+houseBookingRoutes.get('/:id', houseBookingController.getBookingById);
+houseBookingRoutes.delete('/:id', houseBookingController.deleteBooking);
+houseBookingRoutes.put('/:id', houseBookingController.updateBooking);
 
 export default houseBookingRoutes;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/routes/houseListingRoutes.mjs
+++ src/routes/houseListingRoutes.mjs
@@ -1,12 +1,33 @@
 import express from 'express';
 import * as houseController from '../controllers/houseListingController.mjs';
+import { checkRole, isAuthenticated } from '../middleware/authMiddleware.mjs';
+import { createHouseSchema } from '../validators/createHouseSchema.mjs';
+import { updateHouseSchema } from '../validators/updateHouseSchema.mjs';
+import { validateRequest } from '../middleware/requestValidationMiddleware.mjs';
 
 const houseListingRoutes = express.Router();
 
 houseListingRoutes.get('/', houseController.getHouses);
-houseListingRoutes.post('/', houseController.createHouse);
+houseListingRoutes.post(
+  '/',
+  isAuthenticated,
+  checkRole('admin'),
+  validateRequest(createHouseSchema),
+  houseController.createHouse,
+);
 houseListingRoutes.get('/:id', houseController.getHouseById);
-houseListingRoutes.patch('/:id', houseController.updateHouse);
-houseListingRoutes.delete('/:id', houseController.deleteHouse);
+houseListingRoutes.put(
+  '/:id',
+  isAuthenticated,
+  checkRole('admin'),
+  validateRequest(updateHouseSchema),
+  houseController.updateHouse,
+);
+houseListingRoutes.delete(
+  '/:id',
+  isAuthenticated,
+  checkRole('admin'),
+  houseController.deleteHouse,
+);
 
 export default houseListingRoutes;
