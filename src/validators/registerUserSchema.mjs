08c7eaf6dd3ed20dd08009b5a3a0d4550610d36e
lcfranklin <franklinlungu7@gmail.com>
2026-09-10 20:41:51 +0200

    phone validation1

--- src/validators/registerUserSchema.mjs
+++ src/validators/registerUserSchema.mjs
@@ -28,7 +28,7 @@ export const registerUserSchema = Joi.object({
     phone: Joi.string()
     .trim()
     .min(10)
-    .max(13)
+    .max(16)
     .required()
     .messages({
       'any.required': 'Phone number is required',
